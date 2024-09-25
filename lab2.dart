988019aa5603677e7f6a9f3e705902cbd1714a51
saadsh1 <159880857+saadsh1@users.noreply.github.com>
2024-09-25 10:50:35 +0500

    Add files via upload

---
+++ lab2.dart
@@ -0,0 +1,92 @@
+/*void main()
+{
+    int num1=40;
+    int num2=20;
+
+    if (num2==0){
+        print("division by 0 is not allowed");
+    }
+    else {
+        int quotient = num1 ~/ num2;
+        print("the quotient is: $quotient");
+    }
+    }*/
+    
+   /* void main(){
+        int a=10;
+        int b=-a;
+        print(b);
+    } */
+
+    /* void main(){
+        int a=10;
+        int b=20;
+        print (a<=10? "true" : "false" );
+    } */
+
+    /* void main(){
+        int a=10;
+        int b=20;
+        print (a==b);
+    } */
+
+/* void main(){
+        int a=10;
+        int b=20;
+        print (a!=b);
+    } */
+
+/*void main(){
+        int a=10;
+        int b=20;
+        print (a!=b);
+} */
+
+/* int main(){
+    var a;
+    a=50;
+    print(a);
+    return 0;
+} */
+
+/* int main(){
+    int age=18;
+    String eligible = 'citizen';
+    if(age >= 18){
+        print("the person is 18 years old $age");
+        print('the person is also a citizen $eligible');
+    }
+    return 0;
+} 
+
+int main(){
+    int amount=100;
+    String member='premium member';
+  if (member == 'premium member' && amount==100)
+  {
+    print('the member is $member');
+    print('the person is eligibile for a discount of $amount');
+}
+return 0;
+} 
+
+void main() 
+{
+    var a=18;
+    var b=(a>=0)?"number is +" : "number is -";
+    print("$b");
+} 
+
+void main() 
+{
+    var a=19;
+    var b=(a%2==0)?"number is even" : "number is odd";
+    print("$b");
+} */
+
+void main() 
+{
+    var a=100;
+    var b=(a>=100)?"eligible for discount" : "not eligible for discount";
+    print(b);
+}
