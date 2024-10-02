8a3fc8854f09208dbdb7dbe4a379f63f0ffd0a34
saadsh1 <saadshykh69@gmail.com>
2024-10-02 10:16:22 +0500

    git commit --m "new"

--- lab3.dart
+++ lab3.dart
@@ -131,7 +131,74 @@ void main() {
 }  
 
 
+/*void main(){
+  Set<int> emptySet = {}
+  print(emptySet);
+}
+void main(){
+  Set<int> mySet = {10,20,,30}
+  print(mySet);
+}
+void main() {
+  Set<int> mySet = {10,20,30};
+print(mySet.remove(20));
+print(mySet);
+}
 
+void main() {
+  Set<int> mySet = {10,15,30};
+print(mySet.contains(15));
+print(mySet);
+}
+
+void main() {
+  Set<int> mySet = {10,20,30};
+print(mySet.remove(20));
+print(mySet);
+}
+
+void main(){
+  Set<int> otherSet = {10,20,,30}
+  print(otherSet);
+}
+
+void main(){
+  Set<int> mySet = {10,20,30}
+  Set<int> otherSet = {20,30,40}
+  Set<int> unionSet = mySet.union(otherSet)
+  print(unionSet)
+}
+
+void main(){
+  Set<int> mySet = {10,20,30}
+  Set<int> otherSet = {20,30,40}
+  Set<int> unionSet = mySet.intersection(otherSet)
+  print(unionSet)
+}
+
+void main(){
+  Set<int> mySet = {10,20,30};
+  Set<int> otherSet = {20,30,40};
+  Set<int> unionSet = mySet.difference(otherSet);
+  print(unionSet);
+}
+
+void main(){
+  Set<int> mySet = {10,20,30};
+  print(mySet);
+  mySet.forEach((element)=>10,);
+}
+
+void main(){
+  Set<String> employeeSet = {'saad','usama', 'najaf'};
+  List<String> employee_tst = employeeSet.toList();
+  var employeemap = employeeSet.map((value){return '$value';});
+  print(employee_tst);
+  print(employeeSet);
+  print(employeemap);
+}
+
+*/
 
 
 
